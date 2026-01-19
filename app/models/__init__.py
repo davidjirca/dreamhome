0a6b28ab623ef7742392c31f6ff117abebb51527
davidjirca <davidgjirca@gmail.com>
2026-01-19 20:50:19 +0200

    hotfix

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,15 @@
 from app.models.user import User, UserRole
 from app.models.property import Property, PropertyType, PropertyStatus, ListingType
+from app.models.alerts import PropertyPriceHistory, EmailLog
 from app.models.favorite import Favorite
 from app.models.saved_search import SavedSearch, NotificationFrequency
+from app.models.search_analytics import SearchQuery
+
+Property.price_history = relationship(
+    "PropertyPriceHistory",
+    back_populates="property",
+    cascade="all, delete-orphan"
+)
 
 __all__ = [
     "User", 
@@ -12,5 +20,8 @@ __all__ = [
     "ListingType",
     "Favorite",
     "SavedSearch",
-    "NotificationFrequency"
+    "NotificationFrequency",
+    "PropertyPriceHistory",
+    "EmailLog",
+    "SearchQuery"
 ]
